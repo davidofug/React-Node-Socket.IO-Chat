e21dbad6d4ed088297ebc8da1908e7ad5d963ac3
David Wampamba <davidwampamba@gmail.com>
2022-09-27 17:06:47 +0300

    Front end and Server setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log(`User connected ${socket.id}`);
+
+	socket.on("discconect", () => {
+		console.log("User disconnect", socket.id);
+	});
+});
+
+server.listen(3001, () => {
+	console.log("SERVER IS RUNNING");
+});
